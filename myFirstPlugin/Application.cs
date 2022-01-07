00843375a36ac6df24f17d1e32dd8ac620b97764
Musa Mthimunye <52870544+MjMthimunye@users.noreply.github.com>
2022-01-07 23:16:03 +0200

    Add project files.

---
+++ myFirstPlugin/Application.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace myFirstPlugin
+{
+    /// <summary>
+    /// Implements the Revit add-in interface IExternalApplication
+    /// </summary>
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class Application : IExternalApplication
+    {
+        /// <summary>
+        /// Implements the on Shutdown event
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+
+        /// <summary>
+        /// Implements the OnStartup event
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        public Result OnStartup(UIControlledApplication application)
+        {
+            RibbonPanel panel = RibbonPanel(application);
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            if(panel.AddItem(new PushButtonData("jacobian", "jacobian", thisAssemblyPath, "myFirstPlugin.Command")) is PushButton button)
+            {
+                button.ToolTip = "My First Plugin";
+
+                Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(thisAssemblyPath), "Resources", "jacobian.ico"));
+                BitmapImage bitmapImage = new BitmapImage(uri);
+                button.LargeImage = bitmapImage;
+
+            }
+
+            return Result.Succeeded;    
+
+        }
+
+
+        /// <summary>
+        /// Function that creates RibbonPanel
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public RibbonPanel RibbonPanel(UIControlledApplication a)
+        {
+            string tab = "JacobianDev";
+
+            RibbonPanel ribbonPanel = null;
+
+            try
+            {
+                a.CreateRibbonTab(tab);
+            }   
+            catch (Exception ex)
+            {
+               Debug.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                RibbonPanel panel = a.CreateRibbonPanel(tab, "Jacobian");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            List<RibbonPanel> panels = a.GetRibbonPanels(tab);
+            foreach (RibbonPanel p in panels.Where(p => p.Name == "Jacobian"))
+            {
+                ribbonPanel = p;
+            }
+
+            return ribbonPanel;
+
+
+        }
+
+
+    }
+
+}
